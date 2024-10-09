0e668a01a5495a8bb4bafac7e0a77b616b0abe29
vncin <dantas.vinicius.tec@gmail.com>
2024-10-09 20:09:15 -0300

    Atividade09102024

---
+++ views/cadastro_prof.php
@@ -0,0 +1,11 @@
+<?php 
+//Lógica de inserção na tabela professor
+
+/* 
+1º Receber as informações do HTML via POST
+2º preparar a string de inserção 
+3º executar
+4º Exibir uma mensagem de cadastro realizador com sucesso.
+
+*/
+?>
