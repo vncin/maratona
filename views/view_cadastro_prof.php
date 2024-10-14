3e287d97777c711da499c376a0474da152df0490
vncin <dantas.vinicius.tec@gmail.com>
2024-10-14 09:40:34 -0300

    Aula 14102024

---
+++ views/view_cadastro_prof.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/estiloFormCadProf.css">
+    <title>Cadastro de Professor</title>
+</head>
+<body>
+    <main class="principal">
+        <section class="conteudo">
+            <h1>Professor <br/>Cadastre-se agora mesmo no WebRanking</h1>
+            <form action="./cadastro_prof.php" method="POST" class="formulario">
+                <div class="div-nome-matricula">
+                    <div class="form-input">
+                        <label for="nome-completo">Nome Completo</label>
+                        <input type="text" name="nome-completo" id="nome-completo" required>
+                    </div>
+                    <div class="form-input">
+                        <label for="matricula">Matrícula</label>
+                        <input type="text" name="matricula" id="matricula" required>
+                    </div>
+                </div>
+                <div class="form-input-senha">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha" required >
+                </div>
+                <button type="submit" class="botao">Cadastrar</button>
+            </form>
+        </section>
+    </main>
+    <footer>
+         <h2>MiniMaratona MédioTec</h2>
+    </footer>
+</body>
+</html>
